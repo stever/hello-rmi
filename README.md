5e0ed0168362658366484e538fc5065d0ba19cb5
Steven Robertson <steverobertson@gmail.com>
2021-04-23 11:09:36 +0100

    Adding example

---
+++ README.md
@@ -0,0 +1,20 @@
+# Simple RMI Example
+
+## Compile
+
+```bash
+javac *.java
+```
+
+## Start Server
+
+```bash
+rmiregistry &
+java ServerOperation
+```
+
+## Run Client
+
+```bash
+java ClientOperation
+```
---
+++ src/ClientOperation.java
@@ -0,0 +1,24 @@
+import java.io.Console;
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+
+public class ClientOperation {
+
+    public static void main(String[] args) throws MalformedURLException, RemoteException, NotBoundException {
+        String address = "//localhost/MyServer";
+        if (args.length == 1) address = args[0];
+        RMIInterface lookup = (RMIInterface) Naming.lookup(address);
+
+        Console console = System.console();
+        if (console == null) {
+            System.err.println("No console available");
+            return;
+        }
+
+        String line = System.console().readLine("What is your name?");
+        String response = lookup.helloTo(line);
+        System.out.println(response);
+    }
+}
